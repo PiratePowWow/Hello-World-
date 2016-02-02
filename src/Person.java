2257afaeb5e90004f506ae0a5f69fcd9ad46a50d
PiratePowWow <Thi3v3ry>
2016-02-02 11:57:19 -0500

    1.2 Lecture Notes

--- src/Person.java
+++ src/Person.java
@@ -2,9 +2,70 @@
  * Created by PiratePowWow on 2/1/16.
  */
 public class Person {
-    public String Name;
+    public String name;
     public int age;
     public boolean isAlive = true;
-    public String LastName;
+    public String address;
+
+    public Person(){
+
+    }
+
+    public Person(String name, int age){
+        setName(name);
+        setAge(age);
+        setAlive(true);
+        //this(name, age, true) is equivalent, searches for a constructor with matching arguments
+    }
+
+    public Person(String name, int age, boolean isAlive){
+        setName(name);
+        setAge(age);
+        setAlive(isAlive);
+        //this.name = name;
+        //this.age = age;
+        //this.isAlive = isAlive;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        if (isValidName(newName)) {
+            name = newName;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newAge) {
+        if (newAge >= 0){
+            age = newAge;
+        }
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String newAddress) {
+        address = newAddress;
+    }
+
+    public boolean isValidName(String name) {
+        return name.contains(" ");
+    }
 
 }
