732fb0e4bc3057e84e27a71289dc703aec738e69
PiratePowWow <Thi3v3ry>
2016-02-15 11:53:19 -0500

    3.1 - Add Exercises 02 and 03

---
+++ src/Email.java
@@ -0,0 +1,34 @@
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+/**
+ * Created by PiratePowWow on 2/15/16.
+ */
+public class Email implements Comparable {
+    String destination;
+    String subject;
+    String body;
+
+    public Email(String destination, String subject, String body){
+        this.destination = destination;
+        this.subject = subject;
+        this.body = body;
+
+    }
+
+    @Override
+    public int compareTo(Object o){
+        Email e = (Email) o;
+        return destination.compareTo(e.destination);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Email{" +
+                "destination='" + destination + '\'' +
+                ", subject='" + subject + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
